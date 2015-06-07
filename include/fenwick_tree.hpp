0879e88874ec847e35dc7bb24a2a02c2c9dc7380
Maxim Ermilov <ermilov.maxim@gmail.com>
2015-06-07 12:47:09 +0300

    add test for FenwickTree

--- include/fenwick_tree.hpp
+++ include/fenwick_tree.hpp
@@ -4,26 +4,27 @@
 #include "common.hpp"
 
 namespace shelly {
-    template<typename T>
-    class FenwickTree {
-    public:
-        FenwickTree(int n): v(n, 0) {
-        }
-        vector<T> v;
-        void add(int i, T x) {
-            for(; i < (int)v.size(); i |= i+1)
-                v[i] += x;
-        }
-        T sum(int i) const {
-            T r = 0;
-            for(-- i; i >= 0; i = (i & (i+1)) - 1)
-                r += v[i];
-            return r;
-        }
-        T sum(int left, int right) const {
-            return sum(right) - sum(left);
-        }
-    };
+  template<typename T>
+  class FenwickTree {
+  public:
+    FenwickTree(int n): v(n, 0) {
+    }
+    std::vector<T> v;
+    void add(int i, T x) {
+      for(; i < (int)v.size(); i |= i+1)
+	v[i] += x;
+    }
+    T sum(int i) const {
+      i = std::min(i, int(v.size() - 1));
+      T r = 0;
+      for(; i >= 0; i = (i & (i+1)) - 1)
+	r += v[i];
+      return r;
+    }
+    T sum(int left, int right) const {
+      return sum(right) - sum(left - 1);
+    }
+  };
 }
 
 #endif
--- tests/common.cpp
+++ tests/common.cpp
@@ -3,6 +3,7 @@
 #include "math.hpp"
 #include "shelly.hpp"
 #include "avl_tree.hpp"
+#include "fenwick_tree.hpp"
 
 using namespace std;
 using namespace shelly;
@@ -64,4 +65,29 @@ TEST(AVLTree, General) {
     b.insert(t, i * i);
     ASSERT_EQ(b.Find(t), i * i);
     }*/
+}
+
+TEST(FenwickTree, General) {
+  const int kMax = 1000;
+  FenwickTree<int> tree(kMax);
+
+  mt19937_64 _e1(chrono::system_clock::now().time_since_epoch().count());
+  std::uniform_int_distribution<int> dist(0, kMax - 1);
+  vector<int> tt(kMax, 0);
+  for (int i = 0; i < 2000; i++) {
+    int t = dist(_e1);
+    tree.add(t, i * i);
+    tt[t] += i * i;
+
+    t = dist(_e1);
+    int ans = 0;
+    for (int k = 0; k <= t; k++) {
+      ans += tt[k];
+    }
+    ASSERT_EQ(tree.sum(t), ans);
+  }
+  int ans = 0;
+  for (int i = 100; i <= 200; i++)
+    ans += tt[i];
+  ASSERT_EQ(tree.sum(100, 200), ans);
 }
